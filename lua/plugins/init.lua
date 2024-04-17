63a8bf46cf39be0d9c7d96d8864e3a0e27694743
Francois Poizat <francois.poizat@gmail.com>
2024-04-17 13:57:58 +0200

    Use window picker for telescope

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -225,7 +225,7 @@ local default_plugins = {
 
   {
     "nvim-telescope/telescope.nvim",
-    dependencies = { "nvim-treesitter/nvim-treesitter", { "nvim-telescope/telescope-fzf-native.nvim", build = "make" } },
+    dependencies = { "nvim-treesitter/nvim-treesitter", { "nvim-telescope/telescope-fzf-native.nvim", build = "make" }, "s1n7ax/nvim-window-picker"  },
     cmd = "Telescope",
     init = function()
       require("core.utils").load_mappings "telescope"
